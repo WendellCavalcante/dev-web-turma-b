6f6d6a34dba3287bc391bbdb5b56579008c5ebe2
Wendel Nascimento Cavalcante <wendel.nc@dinfor.uniceub>
2026-03-25 21:40:49 -0300

    Publi2503

---
+++ Wendel NC/Aulas/vue_cli/src/components/HomeComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <footer>
+            <p>Bem vindo! De novo {{nome}} Tudo bem? <b>Vue com CLI</b></p>
+        </footer>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: "HomeComponent",
+        data(){
+            return{
+                nome:"Wendel"
+            };
+        }
+    }
+</script>
+
