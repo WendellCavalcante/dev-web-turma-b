261a8da3609ff759a8a9a087927137eaaf56f8ee
Fernando Dias <fernando.dias@sicoob.com.br>
2026-04-01 21:36:17 -0300

    Aula de ciclo de vida e diretivas

--- Professor/Aulas/vue_cli/src/App.vue
+++ Professor/Aulas/vue_cli/src/App.vue
@@ -1,19 +1,37 @@
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>Meu Primeiro App Vue</h1>
+    <p class="subtitulo">Vamos aprender muito!</p>
+    <br />
+    <estilo-component />
+    <br />
     <home-component />
     <hr />
-    <HomeComponent />
+    <usuario-component />
   </div>
 </template>
 
 <script>
 import HomeComponent from "./components/HomeComponent.vue";
+import UsuarioComponent from "./components/UsuarioComponent.vue";
+import EstiloComponent from "./components/EstiloComponent.vue";
 
 export default {
   name: "App",
   components: {
     HomeComponent,
+    UsuarioComponent,
+    EstiloComponent,
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  errorCaptured() {
+    console.log("errorCaptured");
+  },
+  created() {
+    console.log("created");
   },
 };
 </script>
